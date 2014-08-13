740ac4c321dae9d0e07607c2f7e2479aa87703e6
MadMix <mad.mixer0@gmail.com>
2014-08-13 23:20:04 +0400

    Fix line endings in ResourceLoader.cpp

--- src/ResourceLoader.cpp
+++ src/ResourceLoader.cpp
@@ -26,7 +26,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
+ 
 #include "XmlReader.h"
 #include "MLIException.h"
 #include "ResourceLoader.h"
